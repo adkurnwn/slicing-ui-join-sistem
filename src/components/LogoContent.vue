769565635496dbe8a54ad89a4693230f60bd52fd
adkurnwn <adekurniawan@student.uns.ac.id>
2024-11-01 21:47:38 +0700

    names

--- src/components/LogoContent.vue
+++ src/components/LogoContent.vue
@@ -9,19 +9,19 @@
                 <div class="font-sen font-bold leading-8 text-2xl">Featured in</div>
             </div>
             <div class="pl-[13px] flex justify-end pb-2">
-                <img src="@/assets/images/logo/Logo1.png" class="flex-shrink-0 h-8 w-40" />
+                <img src="@/assets/images/logo/logo1.png" class="flex-shrink-0 h-8 w-40" />
             </div>
             <div class="flex pl-0.5 pb-2 flex-col items-center">
-                <img src="@/assets/images/logo/Logo2.png" class="w-[136px] flex-shrink-0 h-8" />
+                <img src="@/assets/images/logo/logo2.png" class="w-[136px] flex-shrink-0 h-8" />
             </div>
             <div class="flex pb-2 flex-col items-center">
-                <img src="@/assets/images/logo/Logo3.png" class="w-[135px] flex-shrink-0 h-8" />
+                <img src="@/assets/images/logo/logo3.png" class="w-[135px] flex-shrink-0 h-8" />
             </div>
             <div class="flex pb-2 flex-col items-center">
-                <img src="@/assets/images/logo/Logo4.png" class="flex-shrink-0 h-8 w-40" />
+                <img src="@/assets/images/logo/logo4.png" class="flex-shrink-0 h-8 w-40" />
             </div>
             <div class="flex pb-2 flex-col items-center">
-                <img src="@/assets/images/logo/Logo5.png" class="w-[136px] flex-shrink-0 h-8" />
+                <img src="@/assets/images/logo/logo5.png" class="w-[136px] flex-shrink-0 h-8" />
             </div>
         </div>
     </div>
